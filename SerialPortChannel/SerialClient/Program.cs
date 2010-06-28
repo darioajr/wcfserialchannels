668f62c29d2f720ad571911e8bfaee6fef5547a5
SriharshaVardhan <none@none>
2010-06-28 18:21:21 +0530

    Added client side inspectors.

--- SerialPortChannel/SerialClient/Program.cs
+++ SerialPortChannel/SerialClient/Program.cs
@@ -6,6 +6,8 @@ using SerialChannel.Binding;
 using System.ServiceModel;
 using System.ServiceModel.Channels;
 using SerialChannel.Binding.Transport;
+using System.ServiceModel.Description;
+using System.ServiceModel.Dispatcher;
 
 namespace SerialClient
 {
@@ -21,7 +23,10 @@ namespace SerialClient
             binding.FactoryPort = "COM8";
 
             ChannelFactory<IFileTrasport> factory = new ChannelFactory<IFileTrasport>(binding);
+            factory.Endpoint.Behaviors.Add(new ConsoleOutputBehavior());
+
             IFileTrasport channel = factory.CreateChannel(new EndpointAddress(uri));
+            
 
             Console.Write("Enter Text or x to quit: ");
             string message;
@@ -37,11 +42,56 @@ namespace SerialClient
         }
     }
 
+
+
+    public class ConsoleOutputMessageInspector : IClientMessageInspector
+    {
+        #region IClientMessageInspector Members
+
+        public void AfterReceiveReply(ref Message reply, object correlationState)
+        {
+            MessageBuffer buffer = reply.CreateBufferedCopy(Int32.MaxValue);
+            reply = buffer.CreateMessage();
+            Console.WriteLine("Received:\n{0}", buffer.CreateMessage().ToString());
+        }
+
+        public object BeforeSendRequest(ref Message request, IClientChannel channel)
+        {
+            MessageBuffer buffer = request.CreateBufferedCopy(Int32.MaxValue);
+            request = buffer.CreateMessage();
+            Console.WriteLine("Sending:\n{0}", buffer.CreateMessage().ToString());
+            return request;
+        }
+
+        #endregion
+    }
+
+    public class ConsoleOutputBehavior : IEndpointBehavior
+    {
+        public void AddBindingParameters(ServiceEndpoint endpoint, BindingParameterCollection bindingParameters)
+        {
+
+        }
+
+        public void ApplyClientBehavior(ServiceEndpoint endpoint, ClientRuntime clientRuntime)
+        {
+            ConsoleOutputMessageInspector inspector = new ConsoleOutputMessageInspector();
+            clientRuntime.MessageInspectors.Add(inspector);
+        }
+
+        public void ApplyDispatchBehavior(ServiceEndpoint endpoint, EndpointDispatcher endpointDispatcher)
+        {
+        }
+
+        public void Validate(ServiceEndpoint endpoint)
+        {
+        }
+    }
+
     [ServiceContract]
     public interface IFileTrasport
     {
         [OperationContract]
         string ProcessReflectRequest(string request);
     }
-
 }
